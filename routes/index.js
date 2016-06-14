fc12692ac1ab2adf9fee4491823fc65b7464c2af
Juan Carlos Choque Quispe <jchoque@agetic.gob.bo>
2016-06-14 13:43:39 -0400

    adicionado squelize

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,47 @@
 var express = require('express');
 var router = express.Router();
 
+//Including dependency
+var Sequelize = require("sequelize");
+
+var sequelize = new Sequelize('demo', 'root', 'mysql', {
+	host: 'localhost',
+  	dialect: 'mysql',
+});
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+	sequelize
+	  .authenticate()
+	  .then(function(err) {
+	    console.log('Connection has been established successfully.');
+	  })
+	  .catch(function (err) {
+	    console.log('Unable to connect to the database:', err);
+	  });
+
+	var User = sequelize.define('user', {
+	  firstName: {
+	    type: Sequelize.STRING,
+	    field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
+	  },
+	  lastName: {
+	    type: Sequelize.STRING
+	  }
+	}, {
+	  freezeTableName: true // Model tableName will be the same as the model name
+	});
+
+	User.sync({force: true}).then(function () {
+	  // Table created
+	  return User.create({
+	    firstName: 'John',
+	    lastName: 'Hancock'
+	  });
+	});
+
   res.render('index', { title: 'Express' });
 });
 
