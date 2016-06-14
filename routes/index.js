bc1ce95cf65a28ddfecc8312a5a432f119833bdc
Juan Carlos Choque Quispe <jchoque@agetic.gob.bo>
2016-06-14 14:10:24 -0400

    pruebas de sequelie

--- routes/index.js
+++ routes/index.js
@@ -45,4 +45,36 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get( '/usern', function(req, res, next) {
+
+	var User = sequelize.define('user', {
+		firstName: {
+	    	type: Sequelize.STRING,
+	    	field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
+	  	},
+	  	lastName: {
+	    	type: Sequelize.STRING
+	  	}
+	}, {
+		freezeTableName: true // Model tableName will be the same as the model name
+	});
+
+	var item1 = User.build({
+		id:5,
+		firstName: 'Juan',
+		lastName: 'Choque'
+	});
+
+	item1.save().catch( function( err ) {
+		if( err ) {
+			console.log( 'Error inserting record ' + err.message );
+		} else {
+			console.log( 'Data successfully inserted' );
+		}
+
+	} );
+
+	res.render('index', { title: 'Express' });
+} );
+
 module.exports = router;
